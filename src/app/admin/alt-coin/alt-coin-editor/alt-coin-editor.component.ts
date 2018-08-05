19c0f559f438c5e25d0e05f92000979100d2a47f
shelbincormsquare <shelbin@cormsquare.com>
2018-08-05 19:22:43 +0530

    2nd commit

---
+++ src/app/admin/alt-coin/alt-coin-editor/alt-coin-editor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alt-coin-editor',
+  templateUrl: './alt-coin-editor.component.html',
+  styleUrls: ['./alt-coin-editor.component.css']
+})
+export class AltCoinEditorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/admin/alt-coin/alt-coin-list/alt-coin-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-alt-coin-list',
+  templateUrl: './alt-coin-list.component.html',
+  styleUrls: ['./alt-coin-list.component.css']
+})
+export class AltCoinListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
